d853cb2b1296ef9d77eb3fa2f23721ec39f3b705
ikghx <admin@ikghx.com>
2022-02-26 09:17:14 +0800

    sync

---
+++ protocols/luci-proto-batman-adv/htdocs/luci-static/resources/protocol/batadv_hardif.js
@@ -0,0 +1,60 @@
+'use strict';
+'require uci';
+'require form';
+'require network';
+
+network.registerPatternVirtual(/^bat.+$/);
+
+return network.registerProtocol('batadv_hardif', {
+	getI18n: function() {
+		return _('B.A.T.M.A.N. advanced Interface');
+	},
+
+	getIfname: function() {
+		return this._ubus('l3_device') || this.sid;
+	},
+
+	getOpkgPackage: function() {
+		return 'batman-adv';
+	},
+
+	isFloating: function() {
+		return false;
+	},
+
+	isVirtual: function() {
+		return false;
+	},
+
+	getDevices: function() {
+		return null;
+	},
+
+	containsDevice: function(ifname) {
+		return (network.getIfnameOf(ifname) == this.getIfname());
+	},
+
+	renderFormOptions: function(s) {
+		var dev = this.getL3Device() || this.getDevice(), o;
+
+
+		// "master" is the batadv interface that we are linking to
+
+		o = s.taboption('general', form.ListValue, 'master', _('B.A.T.M.A.N. interface'),_('This is the batman-adv device where you want to link the physical Device from above to. If this list is empty, then you need to create one first. If you want to route mesh traffic over a wired network device, then please select it from the above Device selector. If you want to assign the batman-adv interface to a Wi-fi mesh then do not select a Device in the Device selector but rather go to the Wireless settings and select this Interface as a network from there.'));
+
+		var uciInterfaces = uci.sections('network', 'interface');
+		for (var i = 0; i < uciInterfaces.length; i++)
+		{
+			if (uciInterfaces[i].proto == 'batadv')
+			{
+				var x=uciInterfaces[i]['.name']; 
+				o.value(x,x);
+			}
+		}
+
+
+		o = s.taboption('general', form.Value, 'mtu', _('Override MTU'));
+		o.placeholder = dev ? (dev.getMTU() || '1536') : '1536';
+		o.datatype    = 'max(9200)';
+	}
+});
