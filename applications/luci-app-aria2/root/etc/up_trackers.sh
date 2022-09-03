30bb563d00e1526fa7170174ac00d6fac0d710a0
ikghx <admin@ikghx.com>
2022-09-03 13:27:25 +0800

    sync

--- applications/luci-app-aria2/root/etc/up_trackers.sh
+++ applications/luci-app-aria2/root/etc/up_trackers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BTlist=`wget -qO- --no-check-certificate https://ngosang.github.io/trackerslist/trackers_all.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
+BTlist=`wget -qO- --no-check-certificate https://trackerslist.com/all.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
 
 uci -q set aria2.@aria2[-1].bt_tracker=$BTlist
 uci -q commit aria2
