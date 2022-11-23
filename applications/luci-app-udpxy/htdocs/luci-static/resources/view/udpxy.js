cfccc5141c8ae5eaba1f68aaf2d1f746e16ed764
ikghx <admin@ikghx.com>
2022-11-23 21:42:41 +0800

    sync

--- applications/luci-app-udpxy/htdocs/luci-static/resources/view/udpxy.js
+++ applications/luci-app-udpxy/htdocs/luci-static/resources/view/udpxy.js
@@ -80,49 +80,41 @@ return view.extend({
 		o.rmempty = false;
 
 		o = s.option(form.Value, 'bind', _('Bind IP/Interface'));
+    o.datatype = 'or(ipaddr, network)';
 		o.placeholder = 'br-lan';
-		o.rmempty = true;
 
 		o = s.option(form.Value, 'port', _('Port'));
 		o.datatype = 'port';
 		o.placeholder = '4022';
-		o.rmempty = true;
 
 		o = s.option(form.Value, 'source', _('Source IP/Interface'));
+    o.datatype = 'or(ipaddr, network)';
 		o.placeholder = 'eth1';
-		o.rmempty = true;
 
 		o = s.option(form.Value, 'max_clients', _('Max clients'));
 		o.datatype = 'range(1,5000)';
 		o.placeholder = '10';
-		o.rmempty = true;
 
 		o = s.option(form.Value, 'log_file', _('Log file'));
 		o.placeholder = '/var/log/udpxy';
-		o.rmempty = true;
 
 		o = s.option(form.Value, 'buffer_size', _('Buffer size'));
-		o.datatype = 'range(4096,2097152)';
-		o.rmempty = true;
+		o.datatype = 'range(4096, 2097152)';
 
 		o = s.option(form.Value, 'buffer_messages', _('Buffer messages'));
-		o.datatype = 'or(-1, and(min(1), uinteger))';
+		o.datatype = 'or(-1, and(min(1),uinteger))';
 		o.placeholder = '-1';
-		o.rmempty = true;
 
 		o = s.option(form.Value, 'buffer_time', _('Buffer time'));
-		o.datatype = 'or(-1, and(min(1), uinteger))';
+		o.datatype = 'or(-1, and(min(1),uinteger))';
 		o.placeholder = '-1';
-		o.rmempty = true;
 
-		o = s.option(form.Flag, 'nice_increment', _('Nice increment'));
-		o.datatype = 'or(and(max(-1), integer),and(min(1), integer))';
-		o.rmempty = true;
+		o = s.option(form.Value, 'nice_increment', _('Nice increment'));
+		o.datatype = 'or(and(max(-1),uinteger), and(min(1),uinteger))';
 
 		o = s.option(form.Value, 'mcsub_renew', _('Multicast subscription renew'));
 		o.datatype = 'or(0, range(30, 64000))';
 		o.placeholder = '300';
-		o.rmempty = true;
 
 		return m.render();
 	}
