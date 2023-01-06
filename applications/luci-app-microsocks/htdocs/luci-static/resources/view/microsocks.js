f4748cd5fda1045cc7d0e607ee959c3686b53fdf
ikghx <admin@ikghx.com>
2023-01-06 23:25:27 +0800

    sync

--- applications/luci-app-microsocks/htdocs/luci-static/resources/view/microsocks.js
+++ applications/luci-app-microsocks/htdocs/luci-static/resources/view/microsocks.js
@@ -46,14 +46,15 @@ return view.extend({
 
 		var m, s, o;
 
-		m = new form.Map('microsocks', _('Micro Socks'), _('Supports only SOCKS5 protocol and forwarding only TCP/IP connections.'));
+		m = new form.Map('microsocks', _('MicroSocks'),
+			_('MicroSocks - multithreaded, small, efficient SOCKS5 server.'));
 
 		s = m.section(form.TypedSection);
 		s.anonymous = true;
 		s.render = function () {
 			poll.add(function () {
 				return L.resolveDefault(getServiceStatus()).then(function (res) {
-					var view = document.getElementById("service_status");
+					var view = document.getElementById('service_status');
 					view.innerHTML = renderStatus(res);
 				});
 			});
@@ -70,11 +71,11 @@ return view.extend({
 		o.rmempty = false;
 
 		o = s.option(form.Value, 'bindaddr', _('Bind address'), _('Specifies the ip to bind to for outgoing connections.'));
-		o.placeholder = '1.1.1.1';
 		o.datatype = 'ipaddr';
 
 		o = s.option(form.Value, 'listenip', _('Listen address'));
-		o.value('127.0.0.1');
+		o.value('0.0.0.0');
+		o.value('::');
 		o.datatype = 'ipaddr';
 		o.rmempty = false;
 
@@ -85,12 +86,18 @@ return view.extend({
 		o = s.option(form.Value, 'user', _('Username'));
 
 		o = s.option(form.Value, 'password', _('Password'));
-
-		o = s.option(form.Flag, 'auth_once', _('Verify only once'), _('The client IP will be added to the whitelist after verifying the password once.'));
+		o.password = true;
 		o.rmempty = false;
+		o.depends({'user': '', '!reverse': true});
+
+		o = s.option(form.Flag, 'auth_once', _('Auth once'),
+			_('Once a specific ip address authed successfully with user/pass, it is added to a whitelist and may use the proxy without auth.'));
+		o.depends({'user': '', '!reverse': true});
 
 		o = s.option(form.Flag, 'disable_log', _('Disable log'));
 		o.rmempty = false;
+
+		o = s.option(form.Flag, 'internet_access', _('Allow access from Internet'));
 
 		return m.render();
 	}
