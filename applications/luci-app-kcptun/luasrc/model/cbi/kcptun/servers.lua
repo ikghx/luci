ec6cc0863bb01904d6c1c9eb39ee8d960cd7de53
ikghx <admin@ikghx.com>
2023-02-14 18:37:48 +0800

    sync

--- applications/luci-app-kcptun/luasrc/model/cbi/kcptun/servers.lua
+++ applications/luci-app-kcptun/luasrc/model/cbi/kcptun/servers.lua
@@ -39,14 +39,14 @@ end
 o = s:option(DummyValue, "_server_address", translate("Server Address"))
 function o.cfgvalue(self, section)
 	local server = m.uci:get("kcptun", section, "server_addr") or "?"
-	local server_port = m.uci:get("kcptun", section, "server_port") or "29900"
+	local server_port = m.uci:get("kcptun", section, "server_port") or "29900-30000"
 	return "%s:%s" % { get_ip_string(server), server_port }
 end
 
 o = s:option(DummyValue, "_listen_addres", translate("Listen Address"))
 function o.cfgvalue(self, section)
 	local local_host = m.uci:get("kcptun", section, "listen_addr") or "0.0.0.0"
-	local local_port = m.uci:get("kcptun", section, "listen_port") or "12984"
+	local local_port = m.uci:get("kcptun", section, "listen_port") or "29900-30000"
 	return "%s:%s" % { get_ip_string(local_host), local_port }
 end
 
