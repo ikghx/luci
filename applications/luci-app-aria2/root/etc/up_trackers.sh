c197ef98c543831692a43a2ad3ae7c7d2e0bec89
ikghx <admin@ikghx.com>
2023-03-14 08:28:48 +0800

    sync

--- applications/luci-app-aria2/root/etc/up_trackers.sh
+++ applications/luci-app-aria2/root/etc/up_trackers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BTlist=`wget -qO- --no-check-certificate https://trackerslist.com/all.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
+BTlist=`wget -qO- https://js.cdn.haah.net/gh/XIU2/TrackersListCollection/best.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
 
 uci -q set aria2.@aria2[-1].bt_tracker=$BTlist
 uci -q commit aria2
