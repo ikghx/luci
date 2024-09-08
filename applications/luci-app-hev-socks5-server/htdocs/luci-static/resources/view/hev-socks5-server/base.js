72113fa879a9bb2b85f5634e1c4229cfed424170
ikghx <admin@ikghx.com>
2024-09-08 10:43:25 -0400

    sync

---
+++ applications/luci-app-hev-socks5-server/htdocs/luci-static/resources/view/hev-socks5-server/base.js
@@ -0,0 +1,68 @@
+'use strict';
+'require view';
+'require form';
+'require poll';
+'require rpc';
+
+var callServiceList = rpc.declare({
+	object: 'service',
+	method: 'list',
+	params: ['name'],
+	expect: { '': {} }
+});
+
+function getServiceStatus() {
+	return L.resolveDefault(callServiceList('hev-socks5-server'), {}).then(function (res) {
+		var isRunning = false;
+		try {
+			isRunning = res['hev-socks5-server']['instances']['instance1']['running'];
+		} catch (e) {}
+		return isRunning;
+	});
+}
+
+function renderStatus(isRunning) {
+    const spanTemp = '<span style="color:%s"><strong>%s</strong></span>';
+
+    return isRunning
+        ? String.format(spanTemp, 'green', _('Running'))
+        : String.format(spanTemp, 'red', _('Not Running'));
+}
+
+return view.extend({
+	load: function () {
+		return Promise.all([
+			getServiceStatus()
+		]);
+	},
+
+	render: function(data) {
+		let isRunning = data[0];
+		var m, s, o;
+
+		m = new form.Map('hev-socks5-server', _('Hev Socks5 Server'), _('HevSocks5Server is a simple, lightweight socks5 server for Unix.'));
+
+		s = m.section(form.TypedSection, 'hev-socks5-server');
+		s.anonymous = true;
+
+		o = s.option(form.DummyValue, '_status', _('Status'));
+		o.rawhtml = true;
+		o.cfgvalue = function () {
+			poll.add(function () {
+				return L.resolveDefault(getServiceStatus()).then(function (res) {
+					var view = document.getElementById('service_status');
+					view.innerHTML = renderStatus(res);
+				});
+			});
+
+			return E('div', { class: 'cbi-section', id: 'status_bar' }, [
+					E('p', { id: 'service_status' }, _('Collecting data...'))
+			]);
+		}
+
+		o = s.option(form.Flag, 'enabled', _('Enabled'));
+		o.rmempty = false;
+
+		return m.render();
+	}
+});
---
+++ applications/luci-app-hev-socks5-tproxy/htdocs/luci-static/resources/view/hev-socks5-tproxy/base.js
@@ -0,0 +1,68 @@
+'use strict';
+'require view';
+'require form';
+'require poll';
+'require rpc';
+
+var callServiceList = rpc.declare({
+	object: 'service',
+	method: 'list',
+	params: ['name'],
+	expect: { '': {} }
+});
+
+function getServiceStatus() {
+	return L.resolveDefault(callServiceList('hev-socks5-tproxy'), {}).then(function (res) {
+		var isRunning = false;
+		try {
+			isRunning = res['hev-socks5-tproxy']['instances']['instance1']['running'];
+		} catch (e) {}
+		return isRunning;
+	});
+}
+
+function renderStatus(isRunning) {
+    const spanTemp = '<span style="color:%s"><strong>%s</strong></span>';
+
+    return isRunning
+        ? String.format(spanTemp, 'green', _('Running'))
+        : String.format(spanTemp, 'red', _('Not Running'));
+}
+
+return view.extend({
+	load: function () {
+		return Promise.all([
+			getServiceStatus()
+		]);
+	},
+
+	render: function(data) {
+		let isRunning = data[0];
+		var m, s, o;
+
+		m = new form.Map('hev-socks5-tproxy', _('Hev Socks5 TProxy'), _('HevSocks5TProxy is a simple, lightweight transparent proxy for Linux.'));
+
+		s = m.section(form.TypedSection, 'hev-socks5-tproxy');
+		s.anonymous = true;
+
+		o = s.option(form.DummyValue, '_status', _('Status'));
+		o.rawhtml = true;
+		o.cfgvalue = function () {
+			poll.add(function () {
+				return L.resolveDefault(getServiceStatus()).then(function (res) {
+					var view = document.getElementById('service_status');
+					view.innerHTML = renderStatus(res);
+				});
+			});
+
+			return E('div', { class: 'cbi-section', id: 'status_bar' }, [
+					E('p', { id: 'service_status' }, _('Collecting data...'))
+			]);
+		}
+
+		o = s.option(form.Flag, 'enabled', _('Enabled'));
+		o.rmempty = false;
+
+		return m.render();
+	}
+});
