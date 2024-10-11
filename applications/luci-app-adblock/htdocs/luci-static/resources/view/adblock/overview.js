0267808ec3a4a25571744c3b8bacba139dd3b06e
ikghx <admin@ikghx.com>
2024-10-11 00:01:34 -0400

    sync

--- applications/luci-app-adblock/htdocs/luci-static/resources/view/adblock/overview.js
+++ applications/luci-app-adblock/htdocs/luci-static/resources/view/adblock/overview.js
@@ -180,9 +180,9 @@ return view.extend({
 				if (sources && info) {
 					for (var i = 0; i < info.active_sources.length; i++) {
 						if (i < info.active_sources.length-1) {
-							src_array += info.active_sources[i].source + ', ';
+							src_array += info.active_sources[i] + ', ';
 						} else {
-							src_array += info.active_sources[i].source
+							src_array += info.active_sources[i]
 						}
 					}
 					sources.textContent = src_array || '-';
@@ -311,7 +311,6 @@ return view.extend({
 
 		o = s.taboption('general', widgets.NetworkSelect, 'adb_trigger', _('Startup Trigger Interface'), _('List of available network interfaces to trigger the adblock start. \
 			Choose \'unspecified\' to use a classic startup timeout instead of a network trigger.'));
-		o.unspecified = true;
 		o.nocreate = true;
 		o.rmempty = true;
 
@@ -320,14 +319,12 @@ return view.extend({
 
 		o = s.taboption('general', widgets.ZoneSelect, 'adb_zonelist', _('Forced Zones'), _('Firewall source zones that should be forced locally.'));
 		o.depends('adb_forcedns', '1');
-		o.unspecified = true;
 		o.multiple = true;
 		o.nocreate = true;
 		o.rmempty = true;
 
 		o = s.taboption('general', form.DynamicList, 'adb_portlist', _('Forced Ports'), _('Firewall ports that should be forced locally.'));
 		o.depends('adb_forcedns', '1');
-		o.unspecified = true;
 		o.multiple = true;
 		o.nocreate = false;
 		o.datatype = 'port';
@@ -484,7 +481,6 @@ return view.extend({
 		o.default = '<em><b>Changes on this tab needs a full adblock service restart to take effect.</b></em>';
 
 		o = s.taboption('adv_report', widgets.DeviceSelect, 'adb_repiface', _('Report Interface'), _('List of available network devices used by tcpdump.'));
-		o.unspecified = true;
 		o.nocreate = false;
 		o.rmempty = true;
 
@@ -589,6 +585,38 @@ return view.extend({
 		for (var i = 0; i < categories.length; i++) {
 			code = categories[i].match(/^(\w+);/)[1].trim();
 			if (code === 'stb') {
+				list = categories[i].match(/^\w+;(.*);/)[1].trim();
+				path = categories[i].match(/^.*;(.*$)/)[1].trim();
+				o.value(path, list);
+			}
+		}
+		o.optional = true;
+		o.rmempty = true;
+
+		o = s.taboption('sources', form.DummyValue, '_sub');
+		o.rawhtml = true;
+		o.default = '<em><b>Hagezi List Selection</b></em>';
+
+		o = s.taboption('sources', form.DynamicList, 'adb_hag_sources', _('Variants'));
+		for (var i = 0; i < categories.length; i++) {
+			code = categories[i].match(/^(\w+);/)[1].trim();
+			if (code === 'hag') {
+				list = categories[i].match(/^\w+;(.*);/)[1].trim();
+				path = categories[i].match(/^.*;(.*$)/)[1].trim();
+				o.value(path, list);
+			}
+		}
+		o.optional = true;
+		o.rmempty = true;
+
+		o = s.taboption('sources', form.DummyValue, '_sub');
+		o.rawhtml = true;
+		o.default = '<em><b>1Hosts List Selection</b></em>';
+
+		o = s.taboption('sources', form.DynamicList, 'adb_hst_sources', _('Variants'));
+		for (var i = 0; i < categories.length; i++) {
+			code = categories[i].match(/^(\w+);/)[1].trim();
+			if (code === 'hst') {
 				list = categories[i].match(/^\w+;(.*);/)[1].trim();
 				path = categories[i].match(/^.*;(.*$)/)[1].trim();
 				o.value(path, list);
