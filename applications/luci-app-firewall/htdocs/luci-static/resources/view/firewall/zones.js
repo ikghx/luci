ab5810a0f92d78a60f0d15b9da54a8cd05e29147
ikghx <admin@ikghx.com>
2026-05-02 12:56:59 -0400

    sync

--- applications/luci-app-firewall/htdocs/luci-static/resources/view/firewall/zones.js
+++ applications/luci-app-firewall/htdocs/luci-static/resources/view/firewall/zones.js
@@ -43,11 +43,6 @@ return view.extend({
 		o = s.option(form.Flag, 'synflood_protect', _('Enable SYN-flood protection'));
 
 		o = s.option(form.Flag, 'drop_invalid', _('Drop invalid packets'));
-		if (L.hasSystemFeature('fullcone')) {
-			o = s.option(form.Flag, 'fullcone', _('Enable FullCone NAT'));
-			if (fw4)
-				o = s.option(form.Flag, 'fullcone6', _('Enable FullCone NAT6'));
-		}
 
 		let p = [
 			s.option(form.ListValue, 'input', _('Input', 'firewall input')),
